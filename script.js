67f8092b446d19af5bcb23ef2fc7458eeccfcb93
Aden Sarlat <200608715@student.georgianc.on.ca>
2025-11-13 16:07:54 -0500

    ToyCar constructor with 11 properties

---
+++ script.js
@@ -0,0 +1,16 @@
+class ToyCar {
+  constructor(model, brand, scale, color, price, carType, topSpeed, engine, horsepower, launchYear, movieAppearance) {
+    this.model = model;
+    this.brand = brand;
+    this.scale = scale;
+    this.color = color;
+    this.price = price;
+    this.carType = carType;
+    this.topSpeed = topSpeed;
+    this.engine = engine;
+    this.horsepower = horsepower;
+    this.launchYear = launchYear;
+    this.movieAppearance = movieAppearance;
+  }
+}
+
